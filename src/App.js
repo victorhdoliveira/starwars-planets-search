7a76a923eb98a058a9582aef084f9099806f7eb3
Victor Hugo <victorhdoliveira@gmail.com>
2023-01-23 10:59:09 -0300

    iniciando a estilização

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import Planets from './page/starWarsPlanets';
 
 function App() {
   return (
-    <Planets />
+    <>
+      <Header />
+      <Planets />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2000px-Star_Wars_Logo.svg.png"
+      alt="starWarsLogo"
+      className="starWarsLogo"
+    />
+  );
+}
